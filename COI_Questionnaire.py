e8cc71962b619fb793521b0b7c4b3655ed4f8aa6
Eduardo Quin <94932412+EdyQuin@users.noreply.github.com>
2022-09-27 13:55:51 -0400

    Update COI_Questionnaire.py

--- COI_Questionnaire.py
+++ COI_Questionnaire.py
@@ -5,19 +5,19 @@ st.image('./SLP_LOGO.png')
 with st.form("form"):
     a_name = st.text_input("Your name")
     b_age = st.number_input("Your age")
-    email = st.text_input("Your email")
-    phone_number = st.text_input("Your phone number")
-    one = st.text_input("Does your spouse agree on how you want to dispose of your home?")
-    two = st.text_input("Does your spouse agree on how you want to dispose of your bank accounts?")
-    three = st.text_input("Does your spouse agree on how you want to dispose of your jewelry?")
-    four = st.text_input("Does your spouse agree on how you want to dispose of your personal property?")
-    five = st.text_input("Does your spouse agree on how you want to dispose of your property?")
-    six = st.text_input("Do you agree with how your spouse wants to dispose of his/her home?")
-    seven = st.text_input("Do you agree with how your spouse wants to dispose of his/her bank accounts")
-    eight = st.text_input("Do you agree with how your spouse wants to dispose of his/her jewelry")
-    nine = st.text_input("Do you agree with how your spouse wants to dispose of his/her personal property")
-    ten = st.text_input("Do you agree with how your spouse wants to dispose of his/her property")
-    submitted = st.form_submit_button("Save in database")
+    c_email = st.text_input("Your email")
+    d_phone_number = st.text_input("Your phone number")
+    e_one = st.text_input("Does your spouse agree on how you want to dispose of your home?")
+    f_two = st.text_input("Does your spouse agree on how you want to dispose of your bank accounts?")
+    g_three = st.text_input("Does your spouse agree on how you want to dispose of your jewelry?")
+    h_four = st.text_input("Does your spouse agree on how you want to dispose of your personal property?")
+    i_five = st.text_input("Does your spouse agree on how you want to dispose of your property?")
+    j_six = st.text_input("Do you agree with how your spouse wants to dispose of his/her home?")
+    k_seven = st.text_input("Do you agree with how your spouse wants to dispose of his/her bank accounts")
+    l_eight = st.text_input("Do you agree with how your spouse wants to dispose of his/her jewelry")
+    m_nine = st.text_input("Do you agree with how your spouse wants to dispose of his/her personal property")
+    n_ten = st.text_input("Do you agree with how your spouse wants to dispose of his/her property")
+    o_submitted = st.form_submit_button("Save in database")
     clear_on_submit=True
 # Connect to Deta Base with your Project Key
 deta = Deta(st.secrets["deta_key"])
@@ -26,9 +26,9 @@ db = deta.Base("LawTech_COI")
 # write the data from the form to the database.
 # You can store any data you want here. Just modify that dictionary below (the entries between the {}).
 if submitted:
-    db.put({"name": a_name, "age": b_age, "email": email, "phone number": phone_number, 
-            "one": one, "two": two, "three": three, "four": four, "five": five,
-           "six": six, "seven": seven, "eight": eight, "nine": nine, "ten": ten})
+    db.put({"name": a_name, "age": b_age, "email": c_email, "phone number": d_phone_number, 
+            "one": e_one, "two": f_two, "three": g_three, "four": h_four, "five": i_five,
+           "six": j_six, "seven": k_seven, "eight": l_eight, "nine": m_nine, "ten": n_ten})
 if submitted:
         st.write("Your answers have been successfully received. For any questions or concerns please contact the office @ asanchez@sanchezlp.com. Please close your browser when you are finished.")
 "---"
