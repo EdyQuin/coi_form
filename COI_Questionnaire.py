af287905b80fa474a25a2ad7b03e6b88701ec274
Eduardo Quin <94932412+EdyQuin@users.noreply.github.com>
2022-09-27 12:01:22 -0400

    Deployment

---
+++ COI_Questionnaire.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from deta import Deta
+
+# Data to be written to Deta Base
+with st.form("form"):
+    name = st.text_input("Your name")
+    age = st.number_input("Your age")
+    email = st.text_input("Your email")
+    phone_number = st.text_input("Your phone number")
+    one = st.text_input("Does you spouse agree on how you want to dispose of your home?")
+    two = st.text_input("Does you spouse agree on how you want to dispose your bank accounts?")
+    three = st.text_input("Does you spouse agree on how you want to dispose your jewelry?")
+    four = st.text_input("Does you spouse agree on how you want to dispose your personal property?")
+    five = st.text_input("Does you spouse agree on how you want to dispose your property?")
+    six = st.text_input("Do you agree with how your spouse wants to dispose of his/her home?")
+    seven = st.text_input("Do you agree with how your spouse wants to dispose of his/her bank accounts")
+    eight = st.text_input("Do you agree with how your spouse wants to dispose of his/her jewelry")
+    nine = st.text_input("Do you agree with how your spouse wants to dispose of his/her personal property")
+    ten = st.text_input("Do you agree with how your spouse wants to dispose of his/her property")
+    submitted = st.form_submit_button("Save in database")
+    clear_on_submit=True
+    
+# Connect to Deta Base with your Project Key
+deta = Deta(st.secrets["deta_key"])
+db = deta.Base("LawTech_COI")
+
+# If the user clicked the submit button,
+# write the data from the form to the database.
+# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
+if submitted:
+    db.put({"name": name, "age": age, "email": email, "phone number": phone_number, 
+            "one": one, "two": two, "three": three, "four": four, "five": five,
+           "six": six, "seven": seven, "eight": eight, "nine": nine, "ten": ten})
+    if submitted:
+        st.write("Your electronic will record has been successfully saved in the database. For any questions or concerns please contact the me @ asanchez@sanchezlp.com")
+    
+"---"
+# This reads all items from the database and displays them to your app.
+# db_content is a list of dictionaries. You can do everything you want with it.
+db_content = db.fetch(query=None, limit=None, last=None).items
+"---"
+st.image('./')
